6ab6f3b7af8b19373a145f1010f125d453261569
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-07-23 11:55:04 +0530

    Create evenpositions.py

---
+++ evenpositions.py
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int n,a[25],i,j,t,count=0;
+printf("\nEnter the how much numbers:");
+scanf("\n%d",&n);
+printf("\nEnter the numbers:");
+for(i=0;i<n;i++)
+{
+   scanf("%d",&a[i]); 
+}
+for(i=0;i<n;i++)
+{
+    if((a[i]%2)==(i%2))
+    {
+        printf("\n%d",a[i]);
+    }
+    else if((a[i]%2)!=(i%2))
+    {
+        printf("\n%d",a[i]);
+    }
+    else
+    {
+        printf("\nNo required number");
+    }
+}
+return 0;
+}
