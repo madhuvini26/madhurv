84cafce4c5143c79ccf88de484877883391e88be
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-07-23 11:18:55 +0530

    Create print L,R.py

---
+++ print L,R.py
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int a,b,i,s;
+    printf("Enter the two numbers:");
+    scanf("%d%d",&a,&b);
+    for(i=0;i<=x||i<=y;i++)
+    {
+        if(i%a==0&&i%b==0)
+        {
+            s=i;
+        }
+    }
+    printf("\nThe smallest number divide by both a&b are:%d",s);
+    }
