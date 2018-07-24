9f94e4b8a970bd9b40a2ba6318f0106651559b0b
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-07-24 14:16:14 +0530

    Update index.py

--- pro_level/index.py
+++ pro_level/index.py
@@ -1,14 +1,27 @@
 n=int(input())
+
 q=int(input())
+
 li=[]
+
 a=[]
+
 for i in range(0,n):
+	
 	li.append(int(input()))
-for i in range(0,q):
+
+	for i in range(0,q):
+	
 	s=0
+	
 	u=int(input())
+	
 	v=int(input())
+	
 	for i in range(u-1,v):
+		
 		s=s^li[i]
+	
 	a.append(s)
-print(a)
+
+	print(a)
