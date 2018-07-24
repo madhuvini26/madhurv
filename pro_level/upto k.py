6cf16c953d983dd499fff3170f94a236a3086357
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-07-24 14:53:58 +0530

    Create upto k.py

---
+++ pro_level/upto k.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=[]
+f=0
+for i in range(n-1):
+	l.append(int(input()))
+k=int(input())
+for i in range(n):
+	if l[i]==l[i+1]:
+		s1=l[i]+l[i+1]
+		if s1==k:
+			f=1
+		print('yes')
+if f!=1:
+print('no')
