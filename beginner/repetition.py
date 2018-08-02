159ef3cdfee5011acb494746700698670cf32854
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-08-02 16:14:27 +0530

    Create repetition.py

---
+++ beginner/repetition.py
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,temp=0;
+printf("\n Enter a,b");
+scanf("%d%d",&a,&b);
+temp=a;
+a=b;
+b=temp;
+printf("swap of a,b %d %d",a,b);
+getch();
+}
