a916b39f11abe81af25c6e0a0cd8488f9867f7c0
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-08-02 16:21:30 +0530

    Create count.py

---
+++ beginner/count.py
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void  main()
+{
+int i,j,g[50],temp,n;
+printf("enter the array size\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n-1;i++)
+{
+for(j=i;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("%d ",a[n-1]);
+getch();
+}
