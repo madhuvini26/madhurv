80465dfaad09595b484b93a9fb9cf1bceae6cb6c
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-08-02 15:32:16 +0530

    Create variables.py

---
+++ beginner/variables.py
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(void)
+{
+   int n,rem=0,rev=0,count=0,i;
+   scanf("%d",&n);
+   while(n!='\0')
+   {
+       rem=n%10;
+       
+       rev=(rev*10)+rem;
+       n=n/10;
+}
+n=rev;
+
+while(n!='\0')
+{
+    rem=n%10;
+    printf("%d ",rem);
+    n=n/10;
+}
+   
+}
