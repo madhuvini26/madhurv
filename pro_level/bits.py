fe9304cc0c4ef0f64dc884b178819150c88c5e8a
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-08-20 15:25:54 +0530

    Create bits.py

---
+++ pro_level/bits.py
@@ -0,0 +1,20 @@
+def bin(n):
+	s=''
+	while(n!=0):
+		s+=str(n%2)
+		n=n//2
+	return(s[::-1])
+def main():
+	n=int(input())
+	(out,l)=('',[])
+	for i in range(2**n):
+		st=bin(i)
+		for j in range(n-len(st)):
+			out+='0'
+		out+=st
+		print(out)
+		out=''
+try:
+ 	 main()
+except:
+  	print('invalid')
