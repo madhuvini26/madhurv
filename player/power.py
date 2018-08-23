f3581dd44cc5e874e552f914f658fabbf86b2633
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-08-23 15:12:46 +0530

    Create power.py

---
+++ player/power.py
@@ -0,0 +1,15 @@
+   #include <stdio.h>
+            void main()
+            {
+            	int n=10;
+            	if(n%2==0)
+            	{
+     
+            	printf("yes");
+     
+            	}
+            	else
+            	{
+            		printf("no");
+            	}
+            }
