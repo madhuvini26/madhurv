c6c932b52e12ffa0ca28a67073bece1fd66cbf18
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-08-29 15:46:58 +0530

    Create prime or not.py

---
+++ beginner/prime or not.py
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int n,i,count=0;
+printf("enter the value\n");
+scanf("%d",&n);
+for(i=2;i<=(n/2);i++)
+{
+if(n%i==0)
+{
+count++;
+}
+if(count==0)
+{
+printf("prime");
+}
+else
+{
+printf("no prime");
+}
+}
+}
