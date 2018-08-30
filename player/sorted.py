fab8153a5adc243c6c24e4b65e960959b9c40783
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-08-30 14:32:55 +0530

    Create sorted.py

---
+++ player/sorted.py
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    char a[20],b[20];
+    int m,n,i,j,count=0;
+    scanf("%d %d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+    scanf("%s",&a[i]);
+    }
+    for(i=0;i<m;i++)
+    {
+        scanf("%s",&b[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(a[i]==b[j])
+            {
+                count++;
+            }
+        }
+    }
+    if(count==m)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    }
+    
