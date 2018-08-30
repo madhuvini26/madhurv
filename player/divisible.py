38acd4775eb3a52dd7e5c18ec6a0dcf1bdf001db
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-08-30 15:43:09 +0530

    Update divisible.py

--- player/divisible.py
+++ player/divisible.py
@@ -1,8 +1,7 @@
 def play_50():
-	n=int(input('Enter n:'))
-	for i in range(2,n//2):
-		if n%i==0:
-			return "yes"
-	return "no"
-  
-play_50()
+n=int(input('Enter n:'))
+for i in range(2,n//2):
+if n%i==0:
+return "yes"
+return "no"
+  play_50()
