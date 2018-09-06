fdc68309912661aad9e8e83f3199a89d73cbaad5
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-09-06 14:44:20 +0530

    Create palindrome S or No.py

---
+++ beginner/set 8/palindrome S or No.py
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char s[100];
+    printf("enter the string\n");
+    scanf("%s",&s);
+    if(s==strrev(s))
+    {
+        printf("palindrome");
+    }
+        else
+        {
+            printf("not palindrome");
+        }
+    }
