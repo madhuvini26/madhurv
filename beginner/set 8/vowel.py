d7495248b5273cb59e1e204b4d37a0f6e705eea1
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-09-06 14:48:28 +0530

    Create vowel.py

---
+++ beginner/set 8/vowel.py
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char s[100];
+int i,count1=0;;
+printf("Enter the string:");
+gets(s);
+for(i=0;i!=strlen(s);i++)
+{
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+count1++;
+}
+if(count1==0)
+printf("no Vowel");
+else
+printf("yes Vowel is present");
+}
