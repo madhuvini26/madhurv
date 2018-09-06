ccd3f0381f42e8993bb99f58b50c21ae35dfabab
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-09-06 15:04:46 +0530

    Create composite.py

---
+++ beginner/set 8/composite.py
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int m,i,count1=0;
+printf("enter the value\n");
+scanf("%d",&m);
+for(i=2;i<=(m/2);i++)
+{
+if(m%i==0)
+{
+count1++;
+}
+if(count1==0)
+{
+printf("prime");
+}
+else
+{
+printf("no prime");
+}
+}
+}
