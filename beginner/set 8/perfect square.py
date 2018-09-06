bea653317c67a90c6b223db3c99051dc5fd8d3d0
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-09-06 15:28:15 +0530

    Create perfect square.py

---
+++ beginner/set 8/perfect square.py
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+	int m,n,c;
+	scanf("%d %d",&m,&n);
+	c=n*m;
+	for(int i=0;i<=c;i++)
+	{
+		if(c==i*i)
+		{
+			printf("Yes");
+			return 0;
+		}
+	}
+	printf("No");
+	getch():
+  }
