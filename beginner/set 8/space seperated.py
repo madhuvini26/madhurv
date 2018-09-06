f571a0a6622664290cf08bb3924041dc1f06e8f9
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-09-06 15:33:34 +0530

    Create space seperated.py

---
+++ beginner/set 8/space seperated.py
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c=0,d;
+    printf("enter the numbers");
+    scanf("%d",&a);
+    while(a!=0)
+    {
+        b=a%10;
+        c=(c*10)+b;
+        a=a/10;
+    }
+    printf("the odd values are:");
+    while(c!=0)
+    {
+        d=c%10;
+        c=c/10;
+        if(d%2!=0)
+        {
+            printf("%d",d);
+        }
+    }
+    }
