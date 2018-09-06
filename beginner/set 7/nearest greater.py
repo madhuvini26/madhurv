c1815796e18ab436cc45cbbd971f4f6cc69ce717
madhuvini26 <41561556+madhuvini26@users.noreply.github.com>
2018-09-06 14:29:05 +0530

    Create nearest greater.py

---
+++ beginner/set 7/nearest greater.py
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,k;
+    printf("enter the value\n");
+    scanf("%d",&n);
+    m=n+1;
+    while(m!=0)
+    {
+        if(m%10==0)
+        {
+            printf("%d",m);
+            break;
+        }
+        m++;
+    }
+  
+}
